dc4108f35be33010a51c81c8eecd28863da40986
Dmitry M <dmitry.m694@gmail.com>
2021-01-07 23:28:40 +0200

    test_message_disappeared_after_adding_product_to_basket + add xfail

--- pages/product_pages.py
+++ pages/product_pages.py
@@ -21,5 +21,6 @@ class ProductPage(BasePage):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be"
 
-
-
+    def check_success_message_is_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared"
--- test_product_page.py
+++ test_product_page.py
@@ -35,3 +35,10 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
+@pytest.mark.xfail(reason="Success message is not disappeared")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.check_success_message_is_disappeared()
