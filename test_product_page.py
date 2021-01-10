e7df841064925467cbb60c0a22dfd3b853abdf4e
Dmitry M <dmitry.m694@gmail.com>
2021-01-10 23:28:28 +0200

    test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 
 from .pages.product_pages import ProductPage
+from .pages.basket_page import BasketPage
 
 #@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -54,3 +55,10 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.can_go_to_login_page_from_product_page()
+
+@pytest.mark.tryc
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.checking_basket()
