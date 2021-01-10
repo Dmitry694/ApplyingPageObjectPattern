e0a22fb5d471c3fad27c90308291e5a50b2655e7
Dmitry M <dmitry.m694@gmail.com>
2021-01-10 23:03:40 +0200

    add class BasketPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -21,3 +21,7 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "login_link_inc")
+
+class BasketPageLocators():
+    BASKET_LINK = (By.CSS_SELECTOR, "#default > header > div.page_inner > div > div.basket-mini.pull-right.hidden-xs > span > a")
+    EMPTY_BASKET_TEXT = (By.CSS_SELECTOR, "#content_inner > p")
