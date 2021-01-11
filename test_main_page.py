85f948c40589b4786e766921902e9c07f86e8be8
Dmitry M <dmitry.m694@gmail.com>
2021-01-11 23:25:52 +0200

    add new class in test_main_page TestLoginFromMainPage

--- test_main_page.py
+++ test_main_page.py
@@ -4,23 +4,28 @@ from .pages.basket_page import BasketPage
 import pytest
 
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_see_login_url(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.should_be_login_url()
+
+    def test_guest_can_see_login_form(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.should_be_login_form()
+
+
 #def test_guest_should_see_login_link(browser):
 #    link = "http://selenium1py.pythonanywhere.com/"
 #    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор #экземпляр драйвера и url адрес
 #    page.open()
 #    page.should_be_login_link()
 
-def test_guest_can_see_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
 
-def test_guest_can_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_form()
 
 def test_guest_can_see_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
