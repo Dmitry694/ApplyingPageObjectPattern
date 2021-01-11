9c3a19806eca2685129d15b950bb2ee101bc90a8
Dmitry M <dmitry.m694@gmail.com>
2021-01-12 01:29:07 +0200

    added new method def register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -15,3 +16,13 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self, email, password, browser):
+        input_mail = browser.find_element(By.CSS_SELECTOR, "#id_registration-email")
+        input_mail.send_keys(email)
+        input_pass1 = browser.find_element(By.CSS_SELECTOR, "#id_registration-password1")
+        input_pass1.send_keys(password)
+        input_pass2 = browser.find_element(By.CSS_SELECTOR, "#id_registration-password2")
+        input_pass2.send_keys(password)
+        register_button = browser.find_element(By.CSS_SELECTOR, "#register_form > button")
+        register_button.click()
