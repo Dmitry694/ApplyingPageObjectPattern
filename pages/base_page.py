4ae675cfd0b08a2caff976b9065d74442ae3623e
Dmitry M <dmitry.m694@gmail.com>
2021-01-12 21:22:53 +0200

    deleting comments and correcting tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,10 +8,9 @@ from selenium.webdriver.support import expected_conditions as EC
 import math
 
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-#        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
--- test_main_page.py
+++ test_main_page.py
@@ -6,25 +6,31 @@ import pytest
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
-    def test_guest_can_see_login_url(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-        page = LoginPage(browser, link)
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
         page.open()
-        page.should_be_login_url()
+        page.go_to_login_page()
 
-    def test_guest_can_see_login_form(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-        page = LoginPage(browser, link)
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
         page.open()
-        page.should_be_login_form()
+        page.should_be_login_link()
 
 
-#def test_guest_should_see_login_link(browser):
-#    link = "http://selenium1py.pythonanywhere.com/"
-#    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор #экземпляр драйвера и url адрес
-#    page.open()
-#    page.should_be_login_link()
+def test_guest_can_see_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
 
+
+def test_guest_can_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
 
 
 def test_guest_can_see_register_form(browser):
@@ -32,6 +38,7 @@ def test_guest_can_see_register_form(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
+
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
